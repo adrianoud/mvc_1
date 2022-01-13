e068498104bba275c67d04193ca2bd10d9009b92
78309 <31667400@qq.com>
2022-01-13 14:34:02 +0800

    Initial commit

---
+++ test1.py
@@ -0,0 +1,56 @@
+quotes = ('A man is not complete until he is married. Then he is finished.',
+          'As I said before, I never repeat myself.',
+          'Behind a successful man is an exhausted woman.',
+          'Black holes really suck...', 'Facts are stubborn things.')
+
+
+class QuoteModel:             # 定义数据模型
+
+    def get_quote(self, n):
+        try:
+            value = quotes[n]
+        except IndexError as err:
+            value = 'Not found!'
+        return value
+
+
+class QuoteTerminalView:    # 定义显示，人机交互的类
+
+    def show(self, quote):
+        print('And the quote is: "{}"'.format(quote))
+
+    def error(self, msg):
+        print('Error: {}'.format(msg))
+
+    def select_quote(self):
+        return input('Which quote number would you like to see?')
+
+
+class QuoteTerminalController:          # 定义逻辑，调用视图类和模型类
+
+    def __init__(self):
+        self.model = QuoteModel()
+        self.view = QuoteTerminalView()
+
+    def run(self):
+        valid_input = False
+        while not valid_input:
+            n = self.view.select_quote()
+            try:
+                n = int(n)
+            except ValueError as err:
+                self.view.error("Incorrect index '{}'".format(n))
+            else:
+                valid_input = True
+        quote = self.model.get_quote(n)
+        self.view.show(quote)
+
+
+def main():
+    controller = QuoteTerminalController()
+    while True:
+        controller.run()
+
+
+if __name__ == '__main__':
+    main()
